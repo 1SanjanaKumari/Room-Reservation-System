5294b768b795acd92669faf529692650bdcda722
{Sanjana} <{12sanjanak@gmail.com}>
2026-02-19 20:46:22 +0530

    Update server.js

--- room-reservation-backend/server.js
+++ room-reservation-backend/server.js
@@ -15,6 +15,11 @@ app.get("/", (req, res) => {
 });
 
 
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/room", require("./routes/roomRoutes"));
+app.use("/api/reservation", require("./routes/reservationRoutes"));
+app.use("/api/dashboard", require("./routes/dashboardRoutes"));
+
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
 );
